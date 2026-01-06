02125df10c242cf9d34da20b31c11ee8fa388a48
Louise Aldridge <louisealdridge88@gmail.com>
2026-01-06 15:11:04 +0000

    removing txt file and testing server response

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("We have a package.json");
+import http from "node:http";
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  res.end("hello from the server!!!");
+});
+
+server.listen(PORT, () => console.log(`server running on port: ${PORT}`));
