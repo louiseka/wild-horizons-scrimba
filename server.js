a9e70f8ed970481501d0a64009ad7232da18b13d
Louise Aldridge <louisealdridge88@gmail.com>
2026-01-08 14:56:19 +0000

    adding error handling

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-  res.end("hello from the server!!!");
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB();
+  if (req.url === "/api" && req.method === "GET") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(destinations));
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 404;
+    res.end(
+      JSON.stringify({
+        error: "not found",
+        message: "The requested route does not exist",
+      })
+    );
+  }
 });
 
 server.listen(PORT, () => console.log(`server running on port: ${PORT}`));
